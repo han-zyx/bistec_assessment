915c6b47ddf4d42a42c15f547965075561391902
Hansaka Tennakoon <connect.hansaka@gmail.com>
2025-03-05 19:14:16 +0530

    create id variable

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,4 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    features {}
-    subscription_id = "a5dc5180-ddcf-4554-b39c-a2123ff1931d" #also can set as a variable
-    
+    features {}    
 }
